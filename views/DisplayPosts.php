4b91afc6901504158e211b05f37e2454951cf300
Fred Madani <madani.fred@gmail.com>
2021-04-27 09:03:47 +0200

    Simple search using the sql like

--- views/DisplayPosts.php
+++ views/DisplayPosts.php
@@ -30,10 +30,15 @@
       </ul>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="?action=login" role="button">Login</a>
+            <form class="nav-link" method="get">
+                <input name="search" type="text"></input>
+            </form>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="?action=register" role="button">Sign Up</a>
+            <a class="nav-link" href="?action=login" role="button">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="?action=register" role="button">Sign Up</a>
         </li>
       </ul>
     </div>
