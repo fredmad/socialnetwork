d7f4455e210d67d253595caab70cb11047352f65
Fred Madani <madani.fred@gmail.com>
2021-04-27 09:17:21 +0200

    Simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,16 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+  $response = $PDO->query("SELECT * FROM user WHERE nickname = '$username' AND password = '$password'");
+  
+  if ($response->rowCount() == 1) {
+    $row = $response->fetch();
+    return $row['id'];
+  } else {
+    return -1;
+  }
+}
